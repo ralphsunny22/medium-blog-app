ffa2e3ff4bc45783edeaec2412acf9ef1303f310
ralphsunny22 <ralphsunny114@gmail.com>
2022-10-11 07:52:27 +0100

    image uploader file

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,7 @@
+# app/uploaders/image_uploader.rb
+class ImageUploader < Shrine
+    Attacher.validate do
+      validate_mime_type %w[image/jpeg image/png image/webp]
+      validate_max_size  1*1024*1024
+    end
+end
